af3eb538200690771d5364efba1e84ee6d435db0
Eugenia Kolodeichuk <80124589+EugeniaKolodeichuk@users.noreply.github.com>
2021-12-20 17:37:40 +0200

    Updates

---
+++ static/js/container.245ee071.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpTikTuk=this.webpackJsonpTikTuk||[]).push([[2],{103:function(n,t,i){"use strict";i.r(t),i.d(t,"default",(function(){return a}));var e=i(74),r=i.n(e),c=i(4);function a(n){var t=n.children;return Object(c.jsx)("div",{className:r.a.container,children:t})}},74:function(n,t,i){n.exports={container:"Container_container__1FLyV"}}}]);
+//# sourceMappingURL=container.245ee071.chunk.js.map
